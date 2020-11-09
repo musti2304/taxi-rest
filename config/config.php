99d1e2ff51fdb141161edc7e591cf4d51b416de0
Mustafa Yousef <musti2304@gmail.com>
2020-11-09 15:31:01 +0100

    Config file

---
+++ config/config.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'url' => 'https://www.gefos.net/serviceinfo-bn/serviceinfo.php?serviceid=',
+    'gps' => '&lat=50.710062&lng=7.117411',
+    'auftraege' => 4,
+    'einstiege' => 9,
+    'wartezeit' => 5,
+    'angemeldet' => 6,
+];
